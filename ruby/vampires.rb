aed4d4c101af1fd13e978442a63e972890fa5555
ajvera <adjove@gmail.com>
2016-08-04 11:39:16 -0700

    Add initial questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+p "What is your name?"
+name = gets.chomp
+
+p "How old are you?"
+age = gets.chomp
+
+p "Our cafeteria serves garlic bread."
+p "Should we order some for you?"
+likes_garlic = gets.chomp
+
+p "Would you like to enroll in the company's health insurance?"
+needs_health_insurance = gets.chomp
