11a7882c080f6bb44fc797d2e9c8b5965eac8746
ajvera <adjove@gmail.com>
2016-08-04 10:54:26 -0700

    Add conditional logic to change data type for [good candidate] to a boolean

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,7 +1,7 @@
 p "What's the hamsters name?"
 name = gets.chomp
 
-p "What is the hamsters volume level?"
+p "What is the hamsters volume level from 1 through 10?"
 ham_vol = gets.chomp.to_i
 
 p "What is the hamsters fur color?"
@@ -9,6 +9,17 @@ ham_color = gets.chomp
 
 p "Is #{name} a good candidate for adoption?"
 can_quality = gets.chomp
+until can_quality == true || can_quality == false
+	if can_quality.downcase == "yes" || can_quality.downcase == "y"
+		then can_quality = true
+	elsif can_quality == "no" || can_quality == "n"
+		then can_quality = false
+	else
+		p "Please type either yes or no."
+		can_quality = gets.chomp
+	end
+end
+		
 
 p "What is the hamsters estimated age?"
 if gets.chomp == ""
