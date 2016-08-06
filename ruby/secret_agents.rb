7af462d144162a4a2f3823c6c2cd34d8bbb70cd9
Adam <adjove@gmail.com>
2016-08-06 12:27:29 -0700

    Add decryption psudeocode and logic

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,21 +1,44 @@
+$encrypted_pass = "zabagoots abonal"
 #Ask for a string (lowercase)
 #Advances every letter of a string one letter forward. 
 #Any space character should remain a space character
 
 p "Please enter your password"
-password = gets.chomp.to_s.downcase
+$password = gets.chomp.to_s.downcase
 
-index = 0
-while index < password.length
-  if password[index] == " "
-    then password[index] = password[index]
-  elsif password[index] == "z"
-    then password[index] = "a"
-  else
-    password[index] = password[index].next!
+def encrypt
+  index = 0
+  while index < $password.length
+    if $password[index] == " "
+      then $password[index] = $password[index]
+    elsif $password[index] == "z"
+      then $password[index] = "a"
+    else
+      $password[index] = $password[index].next!
+    end
+    index += 1
   end
-  index += 1
 end
 
-p password
+$encrypted_pass = $password
 
+#get the encrypted password
+#define alphabet variable
+#compare encrypted password index to alphabet index and 
+#increment backwards 1 
+
+def decrypt
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  index = 0
+  while index < $encrypted_pass.length
+    if $encrypted_pass[index] == " "
+      then $encrypted_pass[index] = $encrypted_pass[index]
+    elsif $encrypted_pass[index] == "a"
+      then $encrypted_pass[index] = "z"
+    else
+      $encrypted_pass[index] = alphabet[alphabet.index($encrypted_pass[index])-1]
+    end
+    index += 1
+  end
+  p $encrypted_pass
+end
