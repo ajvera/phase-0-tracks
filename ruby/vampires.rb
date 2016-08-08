bd6dfcf83fb765be33147ec21b1f0c3bef1534cf
ajvera <adjove@gmail.com>
2016-08-08 08:01:00 -0700

    Add loop for multiple employee surveys

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,58 +3,63 @@ suspicious_name = nil
 suspicious_age = nil
 suspicious_taste = nil
 suspicious_insurance_needs = nil
+i = 0
+p "How many employees will be processed?"
+employees = gets.chomp
 
-p "What is your name?"
-name = gets.chomp
+while i < employees.to_i
 
-if name == "Drake Cula" || name == "Tu Fang"
-  then suspicious_name = true
-else
-  suspicious_name = false
+  p "What is your name?"
+  name = gets.chomp
+  if name == "Drake Cula" || name == "Tu Fang"
+    then suspicious_name = true
+  else
+    suspicious_name = false
+  end
+
+  p "What year were you born?"
+  yob = gets.chomp
+  p "How old are you?"
+  age = gets.chomp
+  if yob + age != current_year
+    then suspicious_age = true
+  else
+    suspicious_age = false
+  end
+
+  p "Our cafeteria serves garlic bread."
+  p "Should we order some for you?"
+  likes_garlic = gets.chomp
+  if likes_garlic.downcase == "no" || likes_garlic.downcase == "n"
+    then suspicious_taste = true
+  elsif likes_garlic.downcase == "yes" || likes_garlic.downcase == "y"
+    then suspicious_taste = false
+  else
+  end
+
+
+  p "Would you like to enroll in the company's health insurance?"
+  needs_health_insurance = gets.chomp
+  if needs_health_insurance.downcase == "no" || needs_health_insurance.downcase == "n"
+    then suspicious_insurance_needs = true
+  elsif needs_health_insurance.downcase == "yes" || needs_health_insurance.downcase =="y"
+    then suspicious_insurance_needs = false
+  else
+  end
+
+  if suspicious_age == false && suspicious_taste == false ||
+  suspicious_age == false && suspicious_insurance_needs == false && suspicious_name == false
+    then p "Probably not a vampire."
+  elsif suspicious_age == true && suspicious_taste == true ||
+  suspicious_age == true && suspicious_insurance_needs == true
+    then p "Probably a vampire."
+  elsif suspicious_age == true && suspicious_taste == true && 
+  suspicious_insurance_needs == true && suspicious_name == false
+    then "Almost certainly a vampire."
+  elsif suspicious_age == false && suspicious_taste == false && 
+  suspicious_insurance_needs == false && suspicious_name == true
+    then
+  else p "Results inconclusive"
+  end
+  i += 1
 end
-
-p "What year were you born?"
-yob = gets.chomp
-
-p "How old are you?"
-age = gets.chomp
-
-if yob + age != current_year
-  then suspicious_age = true
-else
-  suspicious_age = false
-end
-
-p "Our cafeteria serves garlic bread."
-p "Should we order some for you?"
-likes_garlic = gets.chomp
-if likes_garlic.downcase == "yes" || likes_garlic.downcase == "y"
-  then suspicious_taste = false
-elsif likes_garlic.downcase == "no" || likes_garlic.downcase == "n"
-  then suspicious_taste = true
-else
-end
-
-
-p "Would you like to enroll in the company's health insurance?"
-needs_health_insurance = gets.chomp
-if needs_health_insurance.downcase == "yes" || needs_health_insurance.downcase == "y"
-  then suspicious_insurance_needs = false
-elsif needs_health_insurance.downcase == "no" || needs_health_insurance.downcase =="n"
-  then suspicious_insurance_needs = true
-else
-end
-
-if suspicious_name == true
-  then p "Definitely a vampire."
-elsif suspicious_age == false && suspicious_taste == false ||
-suspicious_age == false && suspicious_insurance_needs == false
-  then p "Probably not a vampire."
-elsif suspicious_age == true && suspicious_taste == true ||
-suspicious_age == true && suspicious_insurance_needs == true
-  then p "Probably a vampire."
-elsif suspicious_age == true && suspicious_taste == true && suspicious_insurance_needs == true
-  then "Almost certainly a vampire."
-else p "Results inconclusive"
-end
-    
