f7503b2f469ac3328731935395e7c54811f96788
ajvera <adjove@gmail.com>
2016-08-14 15:51:17 -0700

    Add nested data structure to represent the universe

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,34 @@
+universe = {
+	planetary_systems: {
+		solar_system: {
+			num_of_planets: 8,
+			star_name: 'sun',
+			age_in_gyr: 4.572,
+			confirmed_sentient_life: true
+			known_species: [
+				'Sumatran Rhino',
+				'Leatherback Turtle',
+				'Humans'
+			]
+		}
+		cassiopeia: {
+			num_of_planets: 7,
+			star_name: 'HR 8832',
+			age_in_gyr: 12.66,
+			confirmed_sentient_life: false
+			known_species: []
+		}
+		diskworld: {
+			num_of_planets: 'four elephants on a giant turtles back',
+			star_name: "Great A'Tuin",
+			age_in_gyr: 'unknown'
+			confirmed_sentient_life: true
+			known_species: [
+				'Humans',
+				'Wizards',
+				'Hogfather'
+			]
+		}
+	}
+	makes_sense: false
+}
