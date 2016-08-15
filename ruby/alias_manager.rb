1e6a380ccee4a0308939b90684b8d31ff568bab2
ajvera <adjove@gmail.com>
2016-08-14 23:16:46 -0700

    Pseudocode algorithm and elaborate on single method to accomplish tasks

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,19 +1,53 @@
-#create variable for vowels
-vowels = 'aeiou'
-vowel_array = vowels.split('')
+#create char array variable for vowels
+$vowels = 'aeiou'.split('')
 
-#Create a method that breaks a string into and array of individual words
-def name_chunker(name)
-	name_array = name.split(' ')
-end
-
-#Create a method that swaps the first word and last word of an array for word strings
+#Create a method which turns a str into an array of words then
+#reverses the order of the array and finally joins the words together
+#into one string
 def name_swapper(name)
-	swapped_name = name_chunker(name).reverse!
+	name_array = name.split(' ')
+	swapped_named = name_array.reverse!
+	joined_name = swapped_named.join(' ')
 end
 
-#Create a method that joins a swapped name array into a string again
-def name_joiner(name)
-	joined_name = name_swapper(name).join(' ')
+#define a method which iterates through an array of characters and compare them to 
+#array of vowles then maps any vowle to the next in the array
+def vowel_comp(name)
+	name_ary = name.chars
+	name_index = 0
+	while name_index < name_ary.length
+		if  name_ary[name_index].downcase == "a"
+			name_ary[name_index] = name_ary[name_index].next
+		end
+		name_index +=1
+	end
+	j_name = name_ary.join
 end
 
+#Split name into a two word array
+#Split word array into two nested character arrays
+def nested_name(name)
+	name_array = name.downcase.split(' ')
+	nest = name_array.map do |name|
+		name.chars
+	end
+	nest.map do |char|
+		if char == "a"
+			then char = "e"
+		elsif char == "e"
+			then char = "i"
+		elsif char == "i"
+			then char = "o"
+		elsif char == "o"
+			then char = "u"
+		elsif char == "u"
+			then char = "a"
+		else char = char
+		end
+	end				
+end
+p nested_name("Adam Vera")
+
+#iterate through character arrays and advance each vowel
+#Combine character arrays into two word array
+#swap position of word arrays
