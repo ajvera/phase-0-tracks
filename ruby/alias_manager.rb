d614e014b9635537ee3a9bf82c5fe3a4d47ea39d
ajvera <adjove@gmail.com>
2016-08-15 10:44:40 -0700

    Complete method requirements

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,53 +1,49 @@
-#create char array variable for vowels
-$vowels = 'aeiou'.split('')
-
-#Create a method which turns a str into an array of words then
-#reverses the order of the array and finally joins the words together
-#into one string
-def name_swapper(name)
-	name_array = name.split(' ')
-	swapped_named = name_array.reverse!
-	joined_name = swapped_named.join(' ')
-end
-
-#define a method which iterates through an array of characters and compare them to 
-#array of vowles then maps any vowle to the next in the array
-def vowel_comp(name)
-	name_ary = name.chars
-	name_index = 0
-	while name_index < name_ary.length
-		if  name_ary[name_index].downcase == "a"
-			name_ary[name_index] = name_ary[name_index].next
-		end
-		name_index +=1
-	end
-	j_name = name_ary.join
-end
-
-#Split name into a two word array
-#Split word array into two nested character arrays
-def nested_name(name)
-	name_array = name.downcase.split(' ')
-	nest = name_array.map do |name|
-		name.chars
-	end
-	nest.map do |char|
-		if char == "a"
-			then char = "e"
-		elsif char == "e"
-			then char = "i"
-		elsif char == "i"
-			then char = "o"
-		elsif char == "o"
-			then char = "u"
-		elsif char == "u"
-			then char = "a"
-		else char = char
-		end
-	end				
-end
-p nested_name("Adam Vera")
-
+#BUSINESS LOGIC
 #iterate through character arrays and advance each vowel
-#Combine character arrays into two word array
-#swap position of word arrays
+def next_vowel(char)
+	vowels = 'aeiou'.chars
+	vowel_index = 0
+	while vowel_index < vowels.length
+		if char == vowels[vowel_index]
+			then char = vowels[vowel_index.next]
+			break
+		else
+			char = char
+		end
+		vowel_index += 1
+	end
+	char
+end
+
+def next_consonant(char)
+	consonants = 'bcdfghjklmnpqrstvwxyz'.chars
+	con_index = 0
+	while con_index < consonants.length
+		if char == consonants[con_index]
+			then char = consonants[con_index.next]
+			break
+		else
+			char = char
+		end
+		con_index += 1
+	end
+	char
+end
+
+#Split name into a two word array then into two nested character arrays
+#Swap position of nested arrays and join them together again
+def nested_name(name)
+	name_array = name.downcase.split(' ').map! { |name| name.chars }
+	name_array.each do |sub_arrays|
+		sub_arrays.map! do |char|
+			next_vowel(char)
+		end
+		sub_arrays.map! do |char|
+			next_consonant(char)
+		end
+	end
+	swapped_names = name_array.reverse!
+	joined_names = swapped_names[0].join + ' ' + swapped_names[1].join
+end
+
+#USER INTERFACE
