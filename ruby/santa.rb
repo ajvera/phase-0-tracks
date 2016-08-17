939d614630a42a524df74954f2f525f25dbdfaff
ajvera <adjove@gmail.com>
2016-08-16 22:24:01 -0700

    Initialize Santa

---
+++ ruby/santa.rb
@@ -0,0 +1,20 @@
+class Santa
+
+	def speak()
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+
+	def initialize()
+		puts "Initializing Santa instance..."
+	end
+	
+end
+
+jeff = Santa.new
+p jeff.speak
+p jeff.eat_milk_and_cookies("peanut butter cookie")
+	
