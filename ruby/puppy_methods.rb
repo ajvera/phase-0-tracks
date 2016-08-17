cc5f2fc8e5177d8bec26f49c7e71f4b669c43d4e
ajvera <adjove@gmail.com>
2016-08-16 21:58:04 -0700

    Add Turtle class with 2 instance methods and an initialize method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -28,9 +28,41 @@ class Puppy
 
 end
 
+class Turtle
+
+	def move()
+		puts "*Walks slowly*"
+	end
+
+	def talk(phrase)
+		new_phrase = phrase.chars
+		new_phrase.each do |letter|
+			if letter == " "
+				letter = letter
+			elsif new_phrase.index(letter).even?
+				letter.insert(0,letter * 5)
+			elsif new_phrase.index(letter).odd?
+				letter.insert(0,letter *3)
+			else
+			end
+		end
+		new_phrase.join
+	end
+
+	def initialize()
+		puts "The universe shimmers with anticipation as a new turtle manifests..."
+	end
+
+end
+
+# DRIVER CODE
+
 zeus = Puppy.new
 p zeus.fetch("stick")
 p zeus.speak(5)
 p zeus.roll_over
 p zeus.dog_years(29)
 p zeus.mark_territory
+
+gurt = Turtle.new
+p gurt.talk("Hello there")
