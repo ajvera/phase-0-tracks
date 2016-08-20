28044da6569db8d920cd259b8d532c21bf8362c3
ajvera <adjove@gmail.com>
2016-08-20 14:47:18 -0700

    Add design for Alien class

---
+++ ruby/solo.rb
@@ -0,0 +1,11 @@
+#Class: Alien
+
+#Attributes:
+#- Means of space travel: Array of strings
+#- Number of eyes: Integer
+#- Attitude towards homosapiens: Array of strings
+
+#Methods:
+#- Blink: will return a creepy string describing how the alien blinks
+#- Diplomacy: will interact with earth depending on it's additude towards humanity
+#- Trade: will respond to trade based on a given resource being offered
