7c19475a625e32398e4d8d46f8526404b78b8b6c
ajvera <adjove@gmail.com>
2016-08-21 11:11:03 -0700

    Add calls to shout module methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,14 @@
 module Shout
-	#We'll put some methods here soon, but this code is fine for now!
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def self.yelling_happily(words)
+		'\(^0^)/'  + words + "!!!"
+	end
 end
+
+#Driver code
+p Shout.yelling_happily("This is insane")
+
+p Shout.yell_angrily("I am having so much fun")
