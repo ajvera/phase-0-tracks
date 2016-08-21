663d5d51d7ddd79defec45288060a7b96ee4edb5
ajvera <adjove@gmail.com>
2016-08-21 11:26:07 -0700

    Add new classes to test mixin module syntax, add drive code to test module methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,30 @@
 module Shout
+	def defensive_yell(phrase)
+		puts phrase + " *was yelled defensively*"
+	end
+
+	def yell(phrase)
+		phrase.upcase + "!!!"
+	end
+end
+
+class Rhino
+	include Shout
+end
+
+class Millitary_person
+	include Shout
+end
+#Driver Code
+
+mama_rhino = Rhino.new
+puts mama_rhino.defensive_yell("Ehhhhheehhehhhhhhhggg")
+
+drill_sargent = Millitary_person.new
+puts drill_sargent.yell("would you like to grab some tea")
+
+=begin
+module Shout
 	def self.yell_angrily(words)
 		words + "!!!" + " :("
 	end
@@ -12,3 +38,4 @@ end
 p Shout.yelling_happily("This is insane")
 
 p Shout.yell_angrily("I am having so much fun")
+=end
