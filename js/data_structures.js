002b456484a555969ccd2982ee66fb34e1329afe
ajvera <adjove@gmail.com>
2016-08-26 14:58:56 -0700

    Initial commit, add arrays and push addition values to each

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+var colors = ["blue","magenta","aquamarine","mahogany"];
+var horseNames = ["China White","H","Ed","Horsie the Horse"];
+
+colors.push("glacial blue");
+horseNames.push("Henrietta");
+
