c6daec881ac1ee5c763657902b7ac7fa9ee9b9a0
ajvera <adjove@gmail.com>
2016-09-07 00:00:20 -0500

    add users class to manage table creation and to save the attributes for a given instance of a user to the users table

--- distance_manager/ldl2.rb
+++ distance_manager/ldl2.rb
@@ -2,91 +2,133 @@
 # LDL2 is an app designed to make long distance relationships
 # a more intentional and meaningful experience for all parties.
 
-# BUSINESS LOGIC
+###BUSINESS LOGIC###
 require 'sqlite3'
+require_relative 'users.rb'
 
-# Create ldl2 db which will contain neccisary tables
-$db = SQLite3::Database.new('ldl2.db')
+# Create constant to access LDL2 db
+DB = {:conn => SQLite3::Database.new("ldl2.db")}
 
-# Define and create afinity table
-create_affinity_table =  <<-SQL
-  CREATE TABLE IF NOT EXISTS affinity(
-  a_id INTEGER PRIMARY KEY,
-  date VARCHAR(225),
-  affinity_level REAL,
-  shared_event VARCHAR(225),
-  message VARCHAR(225),
-  user_id INT,
-  FOREIGN KEY (user_id) REFERENCES users(u_id)
-  )
-SQL
-$db.execute(create_affinity_table)
+Users.create_table
 
-# Define and create user table
-create_users_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS users (
-  u_id INTEGER PRIMARY KEY,
-  name VARCHAR(225),
-  email VARCHAR(225),
-  partner_id INT,
-  FOREIGN KEY (partner_id) REFERENCES partners(p_id)
-  ) 
-SQL
-$db.execute(create_users_table)
+###USER INTERFACE###
 
-# Define and create partner table
-create_partners_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS partners (
-  p_id INTEGER PRIMARY KEY,
-  partner_name VARCHAR(225),
-  partner_u_id INT,
-  FOREIGN KEY (partner_u_id) REFERENCES users(u_id)
-  )
-SQL
-$db.execute(create_partners_table)
-
-# CREATE METHOD TO INSERT DATA INTO FEELINGS TABLE
-def data_input()
-  $db.execute("INSERT INTO affinity (date, affinity_level, shared_event, message) VALUES (?,?,?,?)",
-    [$timestamp,$connection_level,$event,$honest_message])
+# User login
+puts "Welcome to LDL2 (the long distance love line)!"
+puts "If you are a new user please type 'new' to register." 
+puts "If you already have an account please enter your username."
+response = gets.chomp
+if response == 'new'
+  puts "What would you like your username to be? Try to pick somethin you will easily remember!"
+  username = gets.chomp
+  puts"What is your email address?"
+  email = gets.chomp
+  puts "What is your long distance partners name?"
+  partner_name = gets.chomp
+  puts "What is your long distance partners email address?"
+  partner_email = gets.chomp
+  user = Users.new(username,email,partner_name,partner_email)
+  user.save
+  puts "Thanks for registering! Now lets move onto the daily Affininator!"
+elsif response != 'new'
+  ### Affinity Questionare
+  puts "On a scale of 0-10, 1 being the lowest and 10 being the highest, how connected to your partner are you feeling today? (decimals are valid)"
+  $affinity_level = gets.chomp.to_f
+  puts "Please enter an event that happened to you recently that you would like to share with your partner."
+  $event = gets.chomp
+  puts "Please send an honest message to your partner."
+  $message = gets.chomp
+  affinity_insert
+  puts "Thanks for sharing with your partner, your responses have been emailed."
+  puts "Have a most excellent day!"
 end
 
-#Create neccisary variables
+
+
+
+
+# Define and create AFFINITY table
+# create_affinity_table =  <<-SQL
+#   CREATE TABLE IF NOT EXISTS affinity(
+#   a_id INTEGER PRIMARY KEY,
+#   date VARCHAR(225),
+#   affinity_level REAL,
+#   shared_event VARCHAR(225),
+#   message VARCHAR(225),
+#   user_id INT,
+#   FOREIGN KEY (user_id) REFERENCES users(u_id)
+#   )
+# SQL
+# DB[:conn].execute(create_affinity_table)
+
+# # Define and create USER table
+# create_users_table = <<-SQL
+#   CREATE TABLE IF NOT EXISTS users (
+#   u_id INTEGER PRIMARY KEY,
+#   name VARCHAR(225),
+#   email VARCHAR(225),
+#   partner_name VARCHAR(225),
+#   partner_email VARCHAR(225)
+#   ) 
+# SQL
+# DB[:conn].execute(create_users_table)
+
+# For simplicity may remove partner table
+# Define and create PARTNER table
+# create_partners_table = <<-SQL
+#   CREATE TABLE IF NOT EXISTS partners (
+#   p_id INTEGER PRIMARY KEY,
+#   partner_name VARCHAR(225),
+#   )
+# SQL
+# $db.execute(create_partners_table)
+
+# method to register user data
+def users_insert
+  DB[:conn].execute("INSERT INTO users (name, email, partner_name, partner_email) VALUES (?,?,?,?)",
+    [$username, $email, $partner_name, $partner_email]) 
+end
+
+# method to register partner data
+# def partners_insert
+#   $db.execute("INSERT INTO (partner_name) VALUES (?)",
+#     [$partner_name])
+# end
+# method to register affinity data
+def affinity_insert
+  DB[:conn].execute("INSERT INTO affinity (date, affinity_level, shared_event, message) 
+    VALUES (?,?,?,?)",
+    [$timestamp,$affinity_level,$event,$message])
+end
+
+#Create timestamp variable
 time = Time.new
 $timestamp = time.month.to_s + '/' + time.day.to_s + '/' + time.year.to_s
 
+# Get user_id and assign to variable
 
-dump = $db.execute("SELECT * FROM affinity")
 
-# USER INTERFACE
 
-# GET USER INPUT FOR TODAY
-puts "On a scale of 0-10, 1 being the lowest and 10 being the highest, how connected to your partner are you feeling today? (decimals are valid)"
-$connection_level = gets.chomp.to_f
-puts "Please enter an event that happened to you recently that you would like to share with your partner."
-$event = gets.chomp
-puts "Please send an honest message to your partner."
-$honest_message = gets.chomp
 
-# INSERT USER INPUT INTO feelings TABLE
-data_input
+
 
 # PRINT SUMMARY OF PARTNERS MESSAGE
-dump.each do |snippet|
-  puts "Your partners connectedness level is #{snippet[2]}."
-  puts "Today your partner wanted to share the following event:\n #{snippet[3]}"
-  puts "They also sent the following message just for you: \n #{snippet[4]}"
-end
+# dump = $db.execute("SELECT * FROM affinity")
+# dump.each do |snippet|
+#   puts "Your partners connectedness level is #{snippet[2]}."
+#   puts "Today your partner wanted to share the following event:\n #{snippet[3]}"
+#   puts "They also sent the following message just for you: \n #{snippet[4]}"
+# end
 
 
 # FUNCTIONALITY TO BE ADDED
 # User login
-  # if new user type new to register
-    # register user information via questionare
-      # verify if partner already exists using combination of email and name
-  # if old user validate login info
-  # if not a new user and user login cannot be validated, exit
-  #proceed to questionare functionality
+  # if new user type new to register - DONE
+    # register user information via questionare - DONE
+      # verify if partner already exists using combination of email and name - NOT DONE
+  # if old user validate login info - NOT DONE
+  # if not a new user and user login cannot be validated, exit - NOT DONE
+  #proceed to questionare functionality - DONE
 #Questionare  
   # if entry for current date exists return last partner submission 
   # exit
---
+++ distance_manager/users.rb
@@ -0,0 +1,37 @@
+require 'sqlite3'
+
+
+class Users
+
+	attr_accessor :name, :email, :partner_name, :partner_email, :u_id
+
+	def initialize(name, email, partner_name, partner_email, u_id=nil)
+		@u_id = u_id
+		@name = name
+		@email = email
+		@partner_name = partner_name
+		@partner_email = partner_email
+	end
+
+	def self.create_table
+		sql = <<-SQL
+			CREATE TABLE IF NOT EXISTS users (
+			  u_id INTEGER PRIMARY KEY,
+			  name VARCHAR(225),
+			  email VARCHAR(225),
+			  partner_name VARCHAR(225),
+			  partner_email VARCHAR(225)
+			  ) 
+			  SQL
+		DB[:conn].execute(sql)
+	end
+
+	def save
+		sql = <<-SQL
+			INSERT INTO users (name, email, partner_name, partner_email) VALUES (?,?,?,?)
+		SQL
+
+		DB[:conn].execute(sql, self.name, self.email, self.partner_name, self.partner_email)
+	end
+
+end
