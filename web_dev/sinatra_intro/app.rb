66884d1d0298eae340b31266c3d6ee41d4a4cd5c
ajvera <adjove@gmail.com>
2016-09-11 16:42:48 -0500

    Add get routes for release 0, provide research findings for release 1

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,32 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET route that displays an address
+get '/contact' do
+  "467 28th St. Awesomeville, CO. 888888"
+end
+
+# write a GET route that takes a persons name as a query param and
+# interpolates it into a statement. Add conditional logic for 
+# instances where no name is present
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Great job, #{params[:name]}!"
+  else
+    "Great job!"
+  end
+end
+
+#write a GET route that adds two numbers and returns the result
+get '/sum/:num1/:num2' do
+  sum = "#{params[:num1]}".to_i + "#{params[:num2]}".to_i
+  s_sum = sum.to_s
+  p s_sum
+end
+
+# Release 1
+#1 No. Ruby On Rails, Cuba, Padrino & Lotus are anothers
+#2 No. MySQL.
+#3 All of the software needed for web dev (including OS). 
